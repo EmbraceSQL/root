9d2dc0a0a9b7725b7b61f75e0429013e218ed980
Will Ballard <wballard@mailframe.net>
2023-11-27 11:46:31 -0600

    tweaking the generation script to have a space for client

--- packages/cli/src/embracesql.ts
+++ packages/cli/src/embracesql.ts
@@ -1,41 +1,50 @@
+import { Command } from "@commander-js/extra-typings";
 import { initializeContext } from "@embracesql/core/src/context";
 import { regenerateFromDatabase } from "@embracesql/core/src/generator";
 import chalk from "chalk";
-import { Command } from "commander";
 import figlet from "figlet";
 
-const program = new Command()
-  .version(process.env.npm_package_version ?? "")
+const program = new Command();
+
+program.version(process.env.npm_package_version ?? "");
+
+program.hook("preAction", () => {
+  process.stderr.write(chalk.blue(figlet.textSync("Embrace SQL")));
+  process.stderr.write("\n");
+});
+
+const generate = program
+  .command("generate")
+  .description("Generate some code from the database.");
+
+generate
+  .command("typescript-node")
+  .description("Typescript code for use in nodejs.")
   .option(
-    "--database [url]",
+    "--database <url>",
     "Connect to this postgres for generation.",
     "postgres://postgres:postgres@localhost:5432/postgres",
   )
   .option(
-    "--sqlScriptsFrom [path]",
+    "--sqlScriptsFrom <path>",
     "Look in this directory for loose SQL scripts.",
     "",
   )
-  .option("--skipSchemas [names...]", "Schemas to skip while generating.", [
+  .option("--skipSchemas <names...>", "Schemas to skip while generating.", [
     "pg_catalog",
     "information_schema",
   ])
-  .parse(process.argv);
-
-async function main() {
-  process.stderr.write(chalk.blue(figlet.textSync("Embrace SQL")));
-  process.stderr.write("\n");
-  const options = program.opts();
-  const context = await initializeContext(options.database);
-  const start = Date.now();
-  const generatedSource = await regenerateFromDatabase({
-    ...context,
-    sqlScriptsFrom: options.sqlScriptsFrom,
-    skipSchemas: options.skipSchemas,
+  .action(async (options) => {
+    const context = await initializeContext(options.database);
+    const start = Date.now();
+    const generatedSource = await regenerateFromDatabase({
+      ...context,
+      sqlScriptsFrom: options.sqlScriptsFrom,
+      skipSchemas: options.skipSchemas,
+    });
+    process.stdout.write(generatedSource);
+    process.stderr.write(chalk.green(`generated in ${Date.now() - start}ms\n`));
+    await context.sql.end();
   });
-  process.stdout.write(generatedSource);
-  process.stderr.write(chalk.green(`generated in ${Date.now() - start}ms\n`));
-  await context.sql.end();
-}
 
-void main();
+void program.parseAsync(process.argv);
