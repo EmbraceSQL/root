bd4a61d1e3be26dd305156bdb8629ad4b198a4ca
Will Ballard <wballard@mailframe.net>
2023-11-28 09:25:22 -0600

    starting up a client

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,9 @@ module.exports = {
   resetModules: true,
   // early exit makes it easier to spot the fail -- maybe a bit perfectionist
   bail: true,
-  projects: ["<rootDir>/packages/test", "<rootDir>/packages/example"],
+  projects: [
+    "<rootDir>/packages/test",
+    "<rootDir>/packages/example",
+    "<rootDir>/packages/client",
+  ],
 };
---
+++ packages/client/src/index.ts
@@ -0,0 +1,72 @@
+import { fetch, Request } from "cross-fetch";
+
+type EmbraceSQLClientProps = {
+  /**
+   * Connect to a server here.
+   */
+  url: string;
+  /**
+   * Any headers you like are passed along to your EmbraceSQL
+   * server.
+   *
+   * These are useful for security, particularly `authorization`
+   * and cors: `origin` and `credentials`.
+   */
+  headers?: Record<string, string>;
+  /**
+   * CORS mode!
+   */
+  mode?: Request["mode"];
+};
+
+type EmbraceSQLClientRequest<P, V = never> = {
+  parameters: P;
+  values: V;
+};
+
+type EmbraceSQLClientResponse<R> = {
+  results: R;
+};
+
+/**
+ * EmbraceSQLClient is the main connection point to the server.
+ *
+ * `new` it up to get started!
+ *
+ * Pass in options, and you are connected.
+ */
+export class EmbraceSQLClient {
+  constructor(public props: EmbraceSQLClientProps) {}
+
+  /**
+   * Directly invoke EmbraceSQL.
+   *
+   * This is used inside code generated client libraries.
+   *
+   * You can call the server yourself this way, but it isn't particularly convenient
+   * compared to using generated react hooks!
+   */
+  async invoke<Parameters, Values, Response>(
+    request: EmbraceSQLClientRequest<Parameters, Values>,
+  ): Promise<EmbraceSQLClientResponse<Response>> {
+    // it's always POST JSON in EmbraceSQL
+    const props = {
+      ...this.props,
+      headers: {
+        ...this.props.headers,
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch(this.props.url, {
+      ...props,
+      method: "POST",
+      cache: "no-cache",
+      redirect: "follow",
+      body: JSON.stringify(request),
+    });
+    // it'll be JSON back or an exception
+    return JSON.parse(
+      await response.json(),
+    ) as unknown as EmbraceSQLClientResponse<Response>;
+  }
+}
