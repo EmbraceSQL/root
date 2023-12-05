29570837734510eb73a7b96e8a315446340862b9
Will Ballard <wballard@mailframe.net>
2023-12-05 06:37:34 -0600

    wip: update test working

--- packages/test-generation/test/hook.test.tsx
+++ packages/test-generation/test/hook.test.tsx
@@ -50,4 +50,46 @@ describe("EmbraceSQL Hooks can", () => {
       expect(result.current?.results).toMatchObject(mockActor),
     );
   });
+  it("update a single record", async () => {
+    // given mocking of the EmbraceSQL HTTP/S server via fetch
+    const mockActor = {
+      actorId: 1,
+      firstName: "Penelope",
+      lastName: "Guiness",
+    };
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        operation: "Public.Actor.byActorid",
+        results: mockActor,
+      }),
+    );
+    const client = new EmbraceSQLClient({ url: "http://localhost" });
+    const wrapper = ({ children }: WithChildren) => (
+      <EmbraceSQLProvider client={client}>{children}</EmbraceSQLProvider>
+    );
+    // here is the intercepted record
+    const { result } = renderHook(
+      () => Public.Tables.Actor.useByActorId({ actorId: 1 }),
+      { wrapper },
+    );
+    await waitFor(() =>
+      expect(result.current?.results).toMatchObject(mockActor),
+    );
+    // intercepted record will update the database -- set up a mock
+    const updatedMockActor = {
+      actorId: 1,
+      firstName: "Alec",
+      lastName: "Guiness",
+    };
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        operation: "Public.Actor.create",
+        results: updatedMockActor,
+      }),
+    );
+    result.current.results!.firstName = "Alec";
+    await waitFor(() =>
+      expect(result.current?.results).toMatchObject(updatedMockActor),
+    );
+  });
 });
