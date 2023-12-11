12fcd14ea688fc10e6ed507c22dd6183fc3a3f4a
Will Ballard <wballard@mailframe.net>
2023-12-11 06:26:02 -0600

    fix: including postgres in workspace

--- packages/postgres/src/generator/pgtype/overrides/oid.ts
+++ packages/postgres/src/generator/pgtype/overrides/oid.ts
@@ -1,35 +1,7 @@
-import { Context } from "../../../context";
-import { PGCatalogType } from "../pgcatalogtype";
+import { PGNumber } from "../base/number";
 import { registerOverride } from "./_overrides";
 
-class OID extends PGCatalogType {
-  typescriptTypeDefinition(context: Context) {
-    console.assert(context);
-    return `
-    export type ${this.typescriptName} = number;
-    `;
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  serializeToPostgres(context: Context, x: any) {
-    // string it
-    if (x) {
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      return `${x}`;
-    } else {
-      return null;
-    }
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  parseFromPostgres(context: Context, x: any) {
-    if (x !== undefined && x !== null) {
-      return +x;
-    } else {
-      return null;
-    }
-  }
-}
+class OID extends PGNumber {}
 
 registerOverride("oid", OID);
 registerOverride("cid", OID);
