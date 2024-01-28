de4637e5501eb25fdef2df8da6c146dca4cef955
Will Ballard <wballard@mailframe.net>
2024-01-28 10:52:39 -0600

    docs: security and middleware

--- packages/postgres/src/middleware/role.ts
+++ packages/postgres/src/middleware/role.ts
@@ -10,7 +10,12 @@ export default async function databaseRole(
   context: MiddlewareContext,
   next: Next,
 ) {
+  // ROLE header is well known -- and generated -- header
   if (context.request?.options?.headers?.ROLE) {
+    // call into the database in the current transaction setting the
+    // role for the remainder of the transaction
+    // the 'actual' query will run with this ROLE set, and this different
+    // permissions that your initial database connection
     await context.sql.unsafe(
       `SET LOCAL SESSION AUTHORIZATION '${context.request.options.headers.ROLE}';`,
     );
